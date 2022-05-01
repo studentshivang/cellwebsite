b4b5d3b78f29af86dd4635042d27abfa7440b1b7
studentshivang <studentshivang>
2022-05-02 01:20:04 +0530

    nabar-hover commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,10 +17,35 @@ const Navbar = ()=>{
                     </ul>
                 </div>
                 </li>
-                <li>For Companies <i className="fa fa-caret-down"></i></li>
-                <li>Student Utilities <i className="fa fa-caret-down"></i></li>
+                <li>For Companies <i className="fa fa-caret-down"></i>
+                    <div className="dropdown_menu">
+                        <ul>
+                        <li>Invitation</li>
+                        <li>Placement Brochure</li>
+                        <li>Placement Procedure</li>
+                        <li>Reaching MMMUT</li>
+                        <li>Placement Statistics/Training</li>
+                        </ul>
+                    </div>
+                </li>
+                <li>Student Utilities <i className="fa fa-caret-down"></i>
+                <div className="dropdown_menu navwidth1">
+                        <ul>
+                        <li className="navwidth">Placement Policy</li>
+                        <li className="navwidth">Login to Superset</li>
+                        <li className="navwidth">Resume Template</li>
+                        </ul>
+                    </div>
+                </li>
                 <li>FAQs</li>
-                <li>Contact Us <i className="fa fa-caret-down"></i></li>
+                <li>Contact Us <i className="fa fa-caret-down"></i>
+                <div className="dropdown_menu navwidth1">
+                        <ul>
+                        <li className="navwidth">Head of T&P, MMMUT</li>
+                        <li className="navwidth">Faculty Coordinators</li>
+                        <li className="navwidth">Students Coordinators</li>
+                        </ul>
+                    </div>                </li>
                 <li><i className="fa fa-linkedin" aria-hidden="true"></i></li>
             </ul>
         </div>
