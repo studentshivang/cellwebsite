895eb19c7892023a1bc389339968b0e42170b505
studentshivang <studentshivang>
2022-06-17 22:21:14 +0530

    Head page

--- src/components/Headtnp.js
+++ src/components/Headtnp.js
@@ -1,12 +1,37 @@
 import Navsm from "./Navsm";
+import "../style/HeadTnp.css";
+import Footer from "./utils/Footer";
+import InvitationText from "./utils/IntvitationText";
+import Email from '../images/email2.png';
+import Phone from '../images/phone-call.png';
 
-const Headtnp =()=>{
-    return(
-        <>
-        <Navsm/>
-        <h1>Welcome to Head T&P Page</h1>
-        </>
-    );
+const Headtnp = () => {
+  return (
+    <>
+      <Navsm />
+      <div className="headTNP">
+        <div className="headImg">
+          <div className="imaGE"></div>
+          <br />
+          <h4 style={{'color':'#457B9D'}}>Prof. V K Dwivedi</h4>
+          <h4>Professor, Training & Placement Cell</h4>
+          <h4>MMMUT, Gorakhpur</h4>
+          <br />
+          <h4><img src={Email} alt="" /> tnp@mmmut.ac.in/ vkdtnp@mmmut.ac.in</h4>
+          <br />
+          <h4><img src={Phone} alt="" />+91 XXXX XXXX XX</h4>
+          <br />
+      
+          <h4 style={{'color':'#457B9D'}}>Home</h4>
+          <br />
+          <h2 style={{ textDecoration: "underline" }}>INVITATION</h2>
+          
+        </div>
+        <InvitationText/>
+        <Footer/>
+      </div>
+    </>
+  );
 };
 
 export default Headtnp;
