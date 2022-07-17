78860ff2491f5984ce054415a5d935ecc2c30dfe
studentshivang <studentshivang>
2022-07-17 16:47:47 +0530

    alumni

--- src/data/Alumndata.js
+++ src/data/Alumndata.js
@@ -1,16 +1,148 @@
 const Alumndata=[
     {
         id:1,
-        image:"",
+        image:"https://picsum.photos/130/130?image=1027",
         name:"Prof. V.K. Dwivedi",
-        post:"Chairman"
+        post:"Chairman",
+        fb:"",
+        in:""
+    },
+    {
+        id:2,
+        image:"https://picsum.photos/130/130?image=1027",
+        name:"Prof. V.K. Dwivedi",
+        post:"Chairman",
+        fb:"",
+        in:""
+    },
+    {
+        id:3,
+        image:"https://picsum.photos/130/130?image=1027",
+        name:"Prof. V.K. Dwivedi",
+        post:"Chairman",
+        fb:"",
+        in:""
+    },
+    {
+        id:4,
+        image:"https://picsum.photos/130/130?image=1027",
+        name:"Prof. V.K. Dwivedi",
+        post:"Chairman",
+        fb:"",
+        in:""
+    },
+    {
+        id:5,
+        image:"https://picsum.photos/130/130?image=1027",
+        name:"Prof. V.K. Dwivedi",
+        post:"Chairman",
+        fb:"",
+        in:""
+    },
+    {
+        id:6,
+        image:"https://picsum.photos/130/130?image=1027",
+        name:"Prof. V.K. Dwivedi",
+        post:"Chairman",
+        fb:"",
+        in:""
+    },
+    {
+        id:7,
+        image:"https://picsum.photos/130/130?image=1027",
+        name:"Prof. V.K. Dwivedi",
+        post:"Chairman",
+        fb:"",
+        in:""
+    },
+    {
+        id:8,
+        image:"https://picsum.photos/130/130?image=1027",
+        name:"Prof. V.K. Dwivedi",
+        post:"Chairman",
+        fb:"",
+        in:""
     },
     {
         id:9,
-        image:"",
-        name:"Dr. Bharti Shukla",
-        post:"Member"
-    }
+        image:"https://picsum.photos/130/130?image=1027",
+        name:"Prof. V.K. Dwivedi",
+        post:"Chairman",
+        fb:"",
+        in:""
+    },
+    {
+        id:10,
+        image:"https://picsum.photos/130/130?image=1027",
+        name:"Prof. V.K. Dwivedi",
+        post:"Chairman",
+        fb:"",
+        in:""
+    },
+    {
+        id:11,
+        image:"https://picsum.photos/130/130?image=1027",
+        name:"Prof. V.K. Dwivedi",
+        post:"Chairman",
+        fb:"",
+        in:""
+    },
+    {
+        id:12,
+        image:"https://picsum.photos/130/130?image=1027",
+        name:"Prof. V.K. Dwivedi",
+        post:"Chairman",
+        fb:"",
+        in:""
+    },
+    {
+        id:13,
+        image:"https://picsum.photos/130/130?image=1027",
+        name:"Prof. V.K. Dwivedi",
+        post:"Chairman",
+        fb:"",
+        in:""
+    },
+    {
+        id:14,
+        image:"https://picsum.photos/130/130?image=1027",
+        name:"Prof. V.K. Dwivedi",
+        post:"Chairman",
+        fb:"",
+        in:""
+    },
+    {
+        id:15,
+        image:"https://picsum.photos/130/130?image=1027",
+        name:"Prof. V.K. Dwivedi",
+        post:"Chairman",
+        fb:"",
+        in:""
+    },
+    {
+        id:16,
+        image:"https://picsum.photos/130/130?image=1027",
+        name:"Prof. V.K. Dwivedi",
+        post:"Chairman",
+        fb:"",
+        in:""
+    },
+    {
+        id:17,
+        image:"https://picsum.photos/130/130?image=1027",
+        name:"Prof. V.K. Dwivedi",
+        post:"Chairman",
+        fb:"",
+        in:""
+    },
+    {
+        id:18,
+        image:"https://picsum.photos/130/130?image=1027",
+        name:"Prof. V.K. Dwivedi",
+        post:"Chairman",
+        fb:"",
+        in:""
+    },
 ];
 
 export default Alumndata;
