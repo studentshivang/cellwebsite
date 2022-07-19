7d1db77065c017c8b53f615c869f3fff5dfbfa0b
Astha <asthabarnwal@gmail.com>
2022-07-19 11:08:23 +0530

    update

--- src/data/Fdata.js
+++ src/data/Fdata.js
@@ -1,55 +1,55 @@
 const Fdata=[
     {
         id:1,
-        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1658156322/faculty/WhatsApp_Image_2022-07-18_at_8.27.48_PM_l3vrv7.jpg",
+        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1658156322/faculty/WhatsApp_Image_2022-07-18_at_8.27.48_PM_l3vrv7.jpg",
         name:"Prof. V.K. Dwivedi",
         post:"Chairman"
     },
     {
         id:2,
-        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1658156106/faculty/rajan2_enxe9f.jpg",
+        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1658156106/faculty/rajan2_enxe9f.jpg",
         name:"Dr. Rajan Mishra",
         post:"Coordinator"
     },
     {
         id:3,
-        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1658156321/faculty/vinay_pic_kzpzb6.jpg",
+        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/e_improve,w_300,h_600,c_thumb,g_auto/v1658156321/faculty/vinay_pic_kzpzb6.jpg",
         name:"Dr. Vinay Kumar Singh",
         post:"Member"
     },
     {
         id:4,
-        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1658156321/faculty/IMGFaculty232_tdonqz.jpg",
+        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1658156321/faculty/IMGFaculty232_tdonqz.jpg",
         name:"Dr. Ravi Shankar",
         post:"Member"
     },
     {
         id:5,
-        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1658156321/faculty/sushil_la696w.jpg",
+        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1658156321/faculty/sushil_la696w.jpg",
         name:"Dr. Shushil Kumar Saroj",
         post:"Member"
     },
     {
         id:6,
-        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1658156321/faculty/shekhar_pyotjw.jpg",
+        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/c_thumb,w_200,g_face/v1658156321/faculty/shekhar_pyotjw.jpg",
         name:"Dr. Shekhar Yadav",
         post:"Member"
     },
     {
         id:7,
-        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1658157385/faculty/ds_singh_cyxgef.jpg",
+        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1658157385/faculty/ds_singh_cyxgef.jpg",
         name:"Dr. D.S. Singh",
         post:"Member"
     },
     {
         id:8,
-        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1658157386/faculty/prashant_nc2tv5.jpg",
+        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1658157386/faculty/prashant_nc2tv5.jpg",
         name:"Dr. Prashant Saini",
         post:"Member"
     },
     {
         id:9,
-        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1658157386/faculty/bharti_fbhqfv.jpg",
+        image:"https://res.cloudinary.com/dpcpegpsn/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1658157386/faculty/bharti_fbhqfv.jpg",
         name:"Dr. Bharti Shukla",
         post:"Member"
     }
