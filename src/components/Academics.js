e084552f2c0cae6e47e97b7d76d2a4c0bf1ea1af
Shivang Dwivedi <83856109+studentshivang@users.noreply.github.com>
2022-07-23 22:43:32 +0530

    Revert "Shivam 1"

--- src/components/Academics.js
+++ src/components/Academics.js
@@ -1,13 +1,12 @@
 import Navsm from "./Navbar/Navsm";
 
-const Academics =()=>{
-    return(
-        <>
-        <Navsm/>
-        <h1>Welcome to Academics Page</h1>
-        
-        </>
-    );
+const Academics = () => {
+  return (
+    <>
+      <Navsm />
+      <h1>Welcome to Academics Page</h1>
+    </>
+  );
 };
 
 export default Academics;
